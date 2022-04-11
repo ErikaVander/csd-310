9f9269b4b7bb6e6d31fa85ae9c7a8fa64508bb0f
ErikaVander <erikaVander123@gmail.com>
2022-04-11 01:24:29 -0700

    added comments to pytech_update

--- module_6/pytech_update.py
+++ module_6/pytech_update.py
@@ -5,10 +5,12 @@
 
 from pymongo import MongoClient
 
+#Connecting to MongoDB database
 url = "mongodb+srv://admin:admin@cluster0.h2zva.mongodb.net/pytech?retryWrites=true&w=majority"
 client = MongoClient(url)
 db = client.pytech
 
+#Calling the find function and displaying each student
 docs = db.students.find({})
 
 print("-- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
@@ -16,8 +18,10 @@ for doc in docs:
    print(doc)
    print("")
 
+#Updating last name of student with id of 1007
 result = db.students.update_one({"student_id": "1007"}, {"$set": {"last_name": "Valteck"}})
 
+#Calling the find_one function and printing the updated students data
 print("-- DISPLAYING STUDENT DOCUMENT FROM find_one() QUERY --")
 doc = db.students.find_one({"student_id": "1007"})
 print(doc)
